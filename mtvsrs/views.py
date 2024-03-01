35d450ebb693c7063ad182e5cd671a6d162d8ffd
oliver xing <sx45@uw.edu>
2024-02-29 18:42:01 -0800

    v1 update

--- mtvsrs/views.py
+++ mtvsrs/views.py
@@ -1,3 +1,24 @@
+from django.shortcuts import render, redirect
+from mtvsrs.models import Movie
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render, redirect
+
+def register_user(request: HttpRequest) -> HttpResponse:
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            success_url = reverse("index")
+            return HttpResponseRedirect(success_url)
+    else:
+        form = UserCreationForm()
+
+    context = {"form": form}
+
+    return render(request, "registration/registration_form.html", context)
 from django.shortcuts import render, get_object_or_404
 from django.db import connection
 from .models import ShowTable, Movie, TvSeries
@@ -60,6 +81,19 @@ def home_page(request):
         'new_release_shows': new_release_shows
     }
     return render(request, "home.html", context)
+
+def search_feature(request):
+    # Check if the request is a post request.
+    if request.method == 'POST':
+        # Retrieve the search query entered by the user
+        search_query = request.POST['search_query']
+        # Filter your model by the search query
+        posts = Movie.objects.filter(name__contains=search_query)
+        return render(request, 'post_search.html', {'query':search_query, 'posts':posts})
+    else:
+        return render(request, 'post_search.html',{})
+
+
 
 
 def show_page(request, show_id):
